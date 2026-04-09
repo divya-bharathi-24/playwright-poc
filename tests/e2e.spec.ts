7f0f180613658819688eb9e1fbb3697deb64c0d5
Divya Bharathi Ravikumar <divya.bharathi@kanini.com>
2026-04-09 14:46:00 +0530

    Update e2e spec

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,16 +1,11 @@
 import { test, expect } from '../fixtures/authFixture';
 import { Header } from '../pages/components/header';
 
-test('End-to-End: Add product and verify cart', async ({ loggedInPage }) => {
+test('E2E: Add product and verify cart', async ({ loggedInPage }) => {
   const header = new Header(loggedInPage);
 
-  // Add product
   await loggedInPage.click('.inventory_item:first-child button');
-
-  // Go to cart
   await header.goToCart();
 
-  // Validate product
-  const product = loggedInPage.locator('.inventory_item_name');
-  await expect(product).toBeVisible();
+  await expect(loggedInPage.locator('.inventory_item_name')).toBeVisible();
 });
